31531e54e0d1ab64fac0a9054a96b4f12e551aeb
zhn3430 <zihan.zhao@cooper.edu>
2024-12-18 19:48:44 -0500

    updated sprint2 code

---
+++ Programming/motion2/Sensors.ino
@@ -0,0 +1,41 @@
+// IR Sensor
+void updateIR() {
+    irData.l2Value = digitalRead(leftIRSensor2);
+    irData.l1Value = digitalRead(leftIRSensor1);
+    irData.mValue = digitalRead(middleIRSensor);
+    irData.r1Value = digitalRead(rightIRSensor1);
+    irData.r2Value = digitalRead(rightIRSensor2);
+}
+
+int identifyJunction() {
+    int l1Value = irData.l1Value;
+    int l2Value = irData.l2Value;
+    int mValue = irData.mValue;
+    int r1Value = irData.r1Value;
+    int r2Value = irData.r2Value;
+    
+    // Identify Left turn
+    if ((l1Value == 0 && l2Value == 0) && (mValue == 0 && (r1Value == 1 || r2Value == 1))) {
+      return 0;
+    }
+    // Identify Right turn
+    if ((r1Value == 0 && r2Value == 0) && (mValue == 0 && (l1Value == 1 || l2Value == 1))) {
+      return 1;
+    }
+    // Identify T stop
+    if ((r1Value == 0 && r2Value == 0 && l1Value == 0 && l2Value == 0 && mValue == 1)) {
+      return 3;  
+    }
+    // Identify Cross OR full box OR ...
+    if ((r1Value == 0 && r2Value == 0 && l1Value == 0 && l2Value == 0 && mValue == 0)) {
+      return 4;
+    }
+    // Identify Offtrack
+    if (r1Value && r2Value && l1Value && l2Value && mValue) {
+      return 5;
+    } 
+    // Unidentified
+    return 6;
+}
+
+// IMU Sensor
---
+++ Programming/motion2/motion2.ino
@@ -0,0 +1,71 @@
+/*
+AS OF 
+12/06/2024 
+ARDUINO UNO HAS BEEN SWITCHED FOR AN ARDUINO MEGA
+*/
+
+#include <rcc.h>
+#include <Wire.h>
+#include <Servo.h>
+
+// <<<<<<<<<<<<<<<<<<<<<<< Setup >>>>>>>>>>>>>>>>>>>>>>
+Servo servo;
+
+MPU6050 IMU;
+
+// Mega pins
+int led = 27;
+int leftIRSensor2 = 26, leftIRSensor1 = 25, middleIRSensor = 24, rightIRSensor1 = 23, rightIRSensor2 = 22; 
+int in1 = 6, in2 = 7, in3 = 8, in4 = 11;
+int enA = 5, enB = 9;
+int leftEncA = 3, leftEncB = 4, rightEncA = 2, rightEncB = 12;
+
+// <<<<<<<<<<<<<<<<<< Gloabl Values >>>>>>>>>>>>>>>>>>>
+// Structs
+struct IRSensorData{
+  int l2Value = 1;
+  int l1Value = 1;
+  int mValue = 1;
+  int r1Value = 1;
+  int r2Value = 1;
+};
+
+Right_Dir_Odom rodom;
+Left_Dir_Odom lodom;
+
+IRSensorData irData;
+
+// Encoders
+volatile long leftEncoderCount = 0;
+
+// <<<<<<<<<<<<<<<<<<<<< Funcitons >>>>>>>>>>>>>>>>>>>>
+// Setup
+void setupMotorController();
+
+// Drive
+void drive(int move_spd);
+void turn(int turn_spd);
+int sensorDrive();
+
+void setup() {
+  setupComponents();
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Driving loop
+  int driving = 1;
+
+  float Kp = 20;
+  float Ki = .01;
+  float Kd = 20 ;
+  
+  while (driving) {
+    // Updating necessary sensors
+    updateIR();
+    int junction = identifyJunction();
+    driving = PIDSensorDrive(Kp, Ki, Kd);
+  }
+
+  finishProcedure();
+}
